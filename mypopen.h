9957cc81ec49c028f66737ebb6ac4283a39a0cc1
JulianeW <JulianeW@users.noreply.github.com>
2015-04-08 20:26:34 +0200

    Update mypopen.h

--- mypopen.h
+++ mypopen.h
@@ -1,5 +1,18 @@
+/*
+ * -------------------------------------------------------------- includes --
+ */
 #include <sys/wait.h>
+#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <errno.h>
+
+/*
+ * ------------------------------------------------------------- prototypes--
+ */
+FILE *popen(const char *command, const char *type);
+
+int pclose(FILE *stream);
+
