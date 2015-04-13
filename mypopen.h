4cb35e49a971bc72e6348123934775151a0f4eb1
reneelsner <rene.elsner@fh-technikum-wien.at>
2015-04-13 19:53:24 +0200

    Update mypopen.h

--- mypopen.h
+++ mypopen.h
@@ -1,18 +1,20 @@
 /*
  * -------------------------------------------------------------- includes --
  */
-#include <sys/wait.h>
-#include <assert.h>
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/types.h>
 #include <errno.h>
+#include <sys/wait.h>
+#include <assert.h>
 
 /*
  * ------------------------------------------------------------- prototypes--
  */
-FILE *popen(const char *command, const char *type);
+ 
+extern FILE *mypopen (const char *p_cCommand, const char *p_cType);
 
 int pclose(FILE *stream);
 
