a9a7fc0d0cc82680e6768906fb41142b83590d7e
JulianeW <JulianeW@users.noreply.github.com>
2015-04-16 11:53:51 +0200

    Update mypopen.h

--- mypopen.h
+++ mypopen.h
@@ -5,10 +5,12 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
-#include <sys/types.h>
+#include <sys/types.h> /* wird für wait(4) gebraucht */
 #include <errno.h>
-#include <sys/wait.h>
+#include <sys/wait.h> /* wird für wait(4) gebraucht */
 #include <assert.h>
+#include <sys/time.h> /* wird für wait(4) gebraucht */
+#include <sys/resource.h> /* wird für wait(4) gebraucht */
 
 /*
  * ------------------------------------------------------------- prototypes--
