4e43bf6807935e02e5615fa841fce33b0f5fe249
JulianeW <JulianeW@users.noreply.github.com>
2015-04-23 17:14:32 +0200

    Update mypopen.h

--- mypopen.h
+++ mypopen.h
@@ -18,16 +18,17 @@
 /*
  * -------------------------------------------------------------- includes --
  */
+ 
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
-#include <sys/types.h> /* wird für wait(4) gebraucht */
+#include <sys/types.h> 
 #include <errno.h>
-#include <sys/wait.h> /* wird für wait(4) gebraucht */
+#include <sys/wait.h> 
 #include <fcntl.h>
-#include <sys/time.h> /* wird für wait(4) gebraucht */
-#include <sys/resource.h> /* wird für wait(4) gebraucht */
+#include <sys/time.h> 
+#include <sys/resource.h> 
 
 /*
  * ------------------------------------------------------------- prototypes--
